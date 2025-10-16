aa67fc51180491bdd6aa8523b72139e2c63104a6
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2025-10-16 18:56:24 +0100

    worked with width

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ for (const fruit of fruits) {
     fruit.style.backgroundColor = words[1];
   });
 }
+
+const line = document.querySelector(".line");
+
+const sizes = document.querySelectorAll(".sizes > li");
+
+for (const size of sizes) {
+  size.addEventListener("click", () => {
+    const words = size.textContent.split(" - ");
+    line.textContent = words[0];
+    line.style.width = words[1];
+  });
+}
